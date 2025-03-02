cfc4d8ea0ee0ae8bc6b917caff45dc4722dedc3b
holysama <97112425+holysama@users.noreply.github.com>
2025-03-02 14:16:34 +0100

    feat: add form pop up with overlay function

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function Book(author, title, pages, read) {
 function addBookToLibrary() {
     
 }
+
+function showForm() {
+
+    let form = document.querySelector(".book-form")
+    let overlay = document.querySelector(".overlay");
+
+    if (form.style.display === "none" && overlay.style.display === "none") {
+        form.style.display = "block";
+        overlay.style.display = "block"
+    } else {
+        form.style.display = "none";
+        overlay.style.display = "none"
+    }
+}
+
